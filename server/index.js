8d9822e0c3e5822cf29a22548b8b64e2081e6bbc
AlifNaufaldo <muhammadalifnaufaldo@gmail.com>
2025-01-31 21:20:08 +0700

    Add initial server setup with environment configuration, package dependencies, and sample data

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const penjualanRoutes = require("./routes/penjualanRoutes");
+const marketingRoutes = require("./routes/marketing");
+
+// routes
+app.use("/penjualan", penjualanRoutes);
+app.use("/marketing", marketingRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
